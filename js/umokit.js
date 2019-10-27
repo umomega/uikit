c5804e8c897dda93e0094ac09dab91e799508a0e
Egemen Berker Kızılcan <egemenkizilcan@gmail.com>
2019-10-27 19:54:06 +0300

    Integrated Bulma elements

---
+++ js/umokit.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    (document.querySelectorAll('.notification .delete') || []).forEach(function($delete) {
+        $notification = $delete.parentNode;
+        $delete.addEventListener('click', function() {
+            $notification.parentNode.removeChild($notification);
+        });
+    });
+});
