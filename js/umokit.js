30985e6ce7be7875513e74b080a100fc28071251
Egemen Berker Kızılcan <egemenkizilcan@gmail.com>
2020-01-31 15:19:26 +0300

    Minor JS Fix

--- js/umokit.js
+++ js/umokit.js
@@ -2,9 +2,7 @@ import $ from 'cash-dom';
 
 $(document).ready(function() {
     $('.notification .delete').on('click', function() {
-        $(this).on('click', function() {
-            $(this).parent().remove();
-        });
+        $(this).closest('.notification').remove();
     });
 
     $('select.pagination-select').on('change', function() {
