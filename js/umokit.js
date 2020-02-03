0c01fbba3dcdb3fd376bf0338cab3749cbdd2905
Egemen Berker Kızılcan <egemenkizilcan@gmail.com>
2020-02-03 21:57:42 +0300

    Implemented the password meter

--- js/umokit.js
+++ js/umokit.js
@@ -1,4 +1,5 @@
 import $ from 'cash-dom';
+import zxcvbn from 'zxcvbn';
 
 $(document).ready(function() {
     $('.notification .delete').on('click', function() {
@@ -9,3 +10,11 @@ $(document).ready(function() {
         window.location.href = $(this).val();
     });
 });
+
+$('input.password-with-meter').each(function() {
+    var input = $(this);
+    input.on('change', function() {
+        var result = zxcvbn(input.val());
+        input.next('div.password-meter').removeClass().addClass('password-meter password-meter--' + result.score);
+    });
+})
